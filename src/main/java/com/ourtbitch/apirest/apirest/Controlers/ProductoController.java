d907f89cee7fd59e9567ed84e8507e35cf87fb05
Misael <156553850+Ourtbitch@users.noreply.github.com>
2024-07-26 19:05:48 -0500

    Update ProductoController.java

--- src/main/java/com/ourtbitch/apirest/apirest/Controlers/ProductoController.java
+++ src/main/java/com/ourtbitch/apirest/apirest/Controlers/ProductoController.java
@@ -1,19 +1,19 @@
-package com.ourtbitch.apirest.apirest.Controlers;
+package com.sergiecode.apirest.apirest.Controllers;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ourtbitch.apirest.apirest.Entiries.Producto;
-import com.ourtbitch.apirest.apirest.Repositories.ProductoRepository;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import com.sergiecode.apirest.apirest.Repositories.ProductoRepository;
+import com.sergiecode.apirest.apirest.Entities.Producto;
 
 @RestController
 @RequestMapping("/productos")
@@ -23,38 +23,40 @@ public class ProductoController {
     private ProductoRepository productoRepository;
 
     @GetMapping
-    public List<Producto> getAllProducts() {
+    public List<Producto> obtenerProductos() {
         return productoRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Producto obtenerProductoPorId(@PathVariable Long id) {
+        return productoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No se encontró el producto con el ID: " + id));
+    }
+
     @PostMapping
-    public Producto createProduc(@RequestBody Producto producto) {
+    public Producto crearProducto(@RequestBody Producto producto) {
         return productoRepository.save(producto);
     }
 
-    @GetMapping("/{id}")
-    public Producto getProductById(@PathVariable Long id) {
-        return productoRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("No se encontro el producto con el id " + id));
-    }
-
     @PutMapping("/{id}")
-    public Producto updateProduc(@PathVariable Long id, @RequestBody Producto detallesProducto) {
-        
-        Producto producto = productoRepository.findById(id).orElseThrow(()-> new RuntimeException("No se encontro el producto con el id "+id));
+    public Producto actualizarProducto(@PathVariable Long id, @RequestBody Producto detallesProducto) {
+        Producto producto = productoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No se encontró el producto con el ID: " + id));
+
         producto.setNombre(detallesProducto.getNombre());
         producto.setPrecio(detallesProducto.getPrecio());
 
         return productoRepository.save(producto);
     }
-        
-    
 
     @DeleteMapping("/{id}")
-    public String deleteProduct(@PathVariable Long id){
-        Producto producto = productoRepository.findById(id).orElseThrow(()-> new RuntimeException("No se encontro el producto con el id "+id));
+    public String borrarProducto(@PathVariable Long id) {
+        Producto producto = productoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("No se encontró el producto con el ID: " + id));
+
         productoRepository.delete(producto);
-        return "El producto con el id: "+id+" fue eliminado correctamente";
+        return "El producto con el ID: " + id + " fue eliminado correctamente";
     }
+
 }
 
